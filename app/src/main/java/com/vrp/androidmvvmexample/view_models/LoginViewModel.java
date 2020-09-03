1c8e0e601e8d574a63f2f8033d4d820a40eb207d
vimal <vimalku637@gmail.com>
2020-09-03 17:25:52 +0530

    initial commit

---
+++ app/src/main/java/com/vrp/androidmvvmexample/view_models/LoginViewModel.java
@@ -0,0 +1,64 @@
+package com.vrp.androidmvvmexample.view_models;
+
+import android.text.TextUtils;
+import android.util.Patterns;
+
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+import androidx.databinding.library.baseAdapters.BR;
+
+import com.vrp.androidmvvmexample.model.User;
+
+public class LoginViewModel extends BaseObservable {
+    private User user;
+
+    private String successMessage = "Login successfully";
+    private String errorMessage = "Email or Password not valid";
+
+    @Bindable
+    private String toastMessage = null;
+
+    public String getToastMessage() {
+        return toastMessage;
+    }
+
+    private void setToastMessage(String toastMessage) {
+        this.toastMessage = toastMessage;
+        notifyPropertyChanged(BR.toastMessage);
+    }
+
+    public void setUserEmail(String email) {
+        user.setEmail(email);
+        notifyPropertyChanged(BR.userEmail);
+    }
+
+    @Bindable
+    public String getUserEmail() {
+        return user.getEmail();
+    }
+
+    @Bindable
+    public String getUserPassword() {
+        return user.getPassword();
+    }
+
+    public void setUserPassword(String password) {
+        user.setPassword(password);
+        notifyPropertyChanged(BR.userPassword);
+    }
+
+    public LoginViewModel() {
+        user = new User("","");
+    }
+
+    public void onLoginClicked() {
+        if (isInputDataValid())
+            setToastMessage(successMessage);
+        else
+            setToastMessage(errorMessage);
+    }
+
+    public boolean isInputDataValid() {
+        return !TextUtils.isEmpty(getUserEmail()) && Patterns.EMAIL_ADDRESS.matcher(getUserEmail()).matches() && getUserPassword().length() > 5;
+    }
+}
